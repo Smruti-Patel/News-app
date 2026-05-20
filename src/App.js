e93af22642e6109949b6b42a79cdc0bb9f9cfa24
Smruti-Patel <116858219+Smruti-Patel@users.noreply.github.com>
2026-05-20 11:35:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ function App() {
     <div>
       <Router>
         <Routes>
-          <Route exact path='/' element={<News   pageSize={5} country="in" category="home"/>}/>
-          <Route exact path='/business' element={<News   pageSize={5} country="in" category="business"/>}/>
-          <Route exact path='/entertainment' element={<News   pageSize={5} country="in" category="entertainment"/>}/>
-          <Route exact path='/general' element={<News   pageSize={5} country="in" category="general"/>}/>
-          <Route exact path='/science' element={<News   pageSize={5} country="in" category="science"/>}/>
-          <Route exact path='/health' element={<News    pageSize={5} country="in" category="health"/>}/>
-          <Route exact path='/sports' element={<News   pageSize={5} country="in" category="sports"/>}/>
-          <Route exact path='/technology' element={<News   pageSize={5} country="in" category="technology"/>}/>
+          <Route exact path='/' element={<News   pageSize={5} country="us" category="home"/>}/>
+          <Route exact path='/business' element={<News   pageSize={5} country="us" category="business"/>}/>
+          <Route exact path='/entertainment' element={<News   pageSize={5} country="us" category="entertainment"/>}/>
+          <Route exact path='/general' element={<News   pageSize={5} country="us" category="general"/>}/>
+          <Route exact path='/science' element={<News   pageSize={5} country="us" category="science"/>}/>
+          <Route exact path='/health' element={<News    pageSize={5} country="us" category="health"/>}/>
+          <Route exact path='/sports' element={<News   pageSize={5} country="us" category="sports"/>}/>
+          <Route exact path='/technology' element={<News   pageSize={5} country="us" category="technology"/>}/>
         </Routes>
       </Router>
     </div>
